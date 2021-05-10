dcb5609859ef3b05d890d6f453603cbc03630b70
julieta96 <julietaextra14@gmail.com>
2021-05-10 13:58:03 -0300

    se cambio el nombre de la bd

---
+++ database.sql
@@ -0,0 +1,25 @@
+create database pokedex;
+use pokedex;
+
+create table Tipo(
+id int,
+descripcion varchar(15),
+primary key (id));
+
+create table Pokemons(
+id int,
+tipo int,
+nombre varchar(20),
+imagen varchar(80),
+primary key (id),
+foreign key (tipo) references Tipo(id));
+
+insert into Tipo(id,descripcion)
+values
+(1,'Electrico'),
+(2,'Agua');
+
+insert into Pokemons(id,tipo, nombre,imagen)
+values
+(1,1,'Pikachu','recursos/img/pokemons/pikachu.png'),
+(2,2,'Squirtle','recursos/img/pokemons/squirtle.png');
